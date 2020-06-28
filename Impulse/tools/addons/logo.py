071037703199488a7d617db5e5d83deb989fd353
ctoANG <67552526+ctoANG@users.noreply.github.com>
2020-06-28 18:01:30 +0200

    Add files via upload

---
+++ Impulse/tools/addons/logo.py
@@ -0,0 +1,10 @@
+# Import modules
+from colorama import Fore
+from colorama import init
+from termcolor import cprint 
+from pyfiglet import figlet_format
+
+# Logo
+cprint('--------------------------------------------', 'blue')
+cprint(figlet_format('Cracked.to/_ANG_', font='standard'), 'blue', attrs=['bold'])
+cprint('--------------------------------------------', 'blue')
